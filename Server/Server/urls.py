b1838ed98ce126a502485a86ed512a3820474bb2
Daniele Viola <daniele.viola02@e-uvt.ro>
2022-05-27 10:09:43 +0300

    small bug fix

--- Server/Server/urls.py
+++ Server/Server/urls.py
@@ -31,6 +31,7 @@ urlpatterns = [
     path('Homepage/Feed/Get', views.search_posts),
     path('manifest.json/', TemplateView.as_view(template_name='manifest.json')),
     path('Post/Load', views.go_to_post),
-    path('Post/', include(views.post_urls)),
-    path('Post/Make', views.make_post)
+    path('Post/Create'),
+    path('/CreatePost', TemplateView.as_view(template_name='manifest.json')),
+    path('Post/Make', views.make_post),
 ]
