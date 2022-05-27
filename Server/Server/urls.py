916429b97b0e2cf349c98e7eee1bc231aefb2efb
Daniele Viola <daniele.viola02@e-uvt.ro>
2022-05-27 10:23:51 +0300

    Auto stash before merge of "posts" and "origin/posts"

--- Server/Server/urls.py
+++ Server/Server/urls.py
@@ -31,10 +31,6 @@ urlpatterns = [
     path('Homepage/Feed/Get', views.search_posts),
     path('manifest.json/', TemplateView.as_view(template_name='manifest.json')),
     path('Post/Load', views.go_to_post),
-<<<<<<< Updated upstream
-    path('CreatePost', TemplateView.as_view(template_name='index.html')),
-=======
     path('CreatePost', TemplateView.as_view(template_name='manifest.json')),
->>>>>>> Stashed changes
     path('Post/Make', views.make_post),
 ]
